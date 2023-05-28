d3a9b75f60d9def0a5b0cb936845dea13520fffb
Amin Tuarshev <atuarshev@gmail.com>
2023-05-28 17:11:02 +0300

    14ПР 10 версия

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
 const errorHandler = require('./middlewares/errorHandler');
 const router = require('./routes');
@@ -12,6 +13,14 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // за 15 минут
+  max: 100, // можно совершить максимум 100 запросов с одного IP
+});
+
+app.use(limiter);
+app.use(helmet());
 
 app.use(router);
 
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,10 @@
 const router = require('express').Router();
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 const auth = require('../middlewares/auth');
 const { signinValidate, signupValidate } = require('../middlewares/validation');
 const { createUser, login } = require('../controllers/users');
 const { NotFound } = require('../utils/errors/errors');
-
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // за 15 минут
-  max: 100, // можно совершить максимум 100 запросов с одного IP
-});
-
-router.use(limiter);
-router.use(helmet());
 
 router.post('/signin', signinValidate, login);
 router.post('/signup', signupValidate, createUser);
